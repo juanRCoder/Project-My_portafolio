9c73780f1a3e258c50cb2bf998b686b4c9abd045
JuanRCoder <guillecarck@gmail.com>
2023-10-11 13:08:28 -0500

    feat: update files for portafolio

--- script.js
+++ script.js
@@ -1,4 +1,22 @@
+const boxAbout = document.querySelector('.skills_about_box');
+const boxSkills = document.querySelector('.skills_about_contenedor');
+
 window.addEventListener('scroll', ()=>{
     let scrollY = window.scrollY;
-    console.log(scrollY);
+    // console.log(scrollY);
+    setTimeout(() => {
+
+        // section: ABOUT * SKILLS
+        if (scrollY >  400 && scrollY <  1130) {
+            boxAbout.classList.add('skills_about_box_change');
+            boxSkills.classList.add('skills_about_contenedor_change');
+        } else {
+            boxAbout.classList.remove('skills_about_box_change');
+            boxSkills.classList.remove('skills_about_contenedor_change');
+        }
+
+        // section: PROJECTS
+    }, 100);
 });
+
+// skills_about_contenedor
