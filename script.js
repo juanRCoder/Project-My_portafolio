7d124fb8241bdd44749257e9b83c255ac0b6887a
JuanRCoder <guillecarck@gmail.com>
2023-10-16 11:21:32 -0500

    update portafolio

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 const boxAbout = document.querySelector('.skills_about_box');
 const boxSkills = document.querySelector('.skills_about_contenedor');
 
+
+const btnModal = document.querySelector('.btn_modal');
+const btnExit = document.querySelector('.btn_exit');
+const modal = document.querySelector('.model');
+
 window.addEventListener('scroll', ()=>{
     let scrollY = window.scrollY;
     // console.log(scrollY);
     setTimeout(() => {
-
-        // section: ABOUT * SKILLS
         if (scrollY >  400 && scrollY <  1130) {
             boxAbout.classList.add('skills_about_box_change');
             boxSkills.classList.add('skills_about_contenedor_change');
@@ -14,9 +17,5 @@ window.addEventListener('scroll', ()=>{
             boxAbout.classList.remove('skills_about_box_change');
             boxSkills.classList.remove('skills_about_contenedor_change');
         }
-
-        // section: PROJECTS
     }, 100);
 });
-
-// skills_about_contenedor
