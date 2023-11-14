7f34671d3bf09f5ea27d4175162d0a539ccfb897
JuanRCoder <guillecarck@gmail.com>
2023-11-14 17:56:56 -0500

    feat: update portfolio

--- script.js
+++ script.js
@@ -11,4 +11,28 @@ $(document).ready(function() {
     $('.caja').on('click', function() {
         $(this).toggleClass("girada");
     })
+
+
+    // animacion en formulario de solicitud
+    $('.input_container').on('mousedown', function() {
+        $(this).toggleClass('input_container_change');
+    });
+
+
+    // contando la cantidad de caracteres en el textarea
+    $('#textarea').on('input', function() {
+        const maxLength = parseInt($(this).attr('maxlength'));
+        const currentLength = $(this).val().length;
+        const charactersLeft = maxLength - currentLength;
+        const contador = $('#contadorCaracteres');
+
+        contador.text((maxLength - charactersLeft) + '/' + maxLength);
+
+        if (charactersLeft < 0) {
+            contador.css('color', 'red');
+        } else {
+            contador.css('color', ''); // Restablecer el color predeterminado
+        }
+    });
 });
+
