d92d2efa44c1cbfdaf19336d87f997e0d4f5ec53
JuanRCoder <guillecarck@gmail.com>
2023-11-14 00:11:10 -0500

    feat: update portfolio

--- script.js
+++ script.js
@@ -1,20 +1,14 @@
 $(document).ready(function() {
-    const boxAbout = $('.skills_about_box');
-    const boxSkills = $('.skills_about_contenedor');
-    const project = $('.md');
-    const projectExit = $('.project_modal_content_exit');
-    const modal = $('.project_modal');
-
-    $('.project_modal').hide();
-    project.on('click', () => {
-        $('.project_modal').show();
+    
+    $(document).on('click', function(event) {
+        // Verifica si el clic ocurrió fuera de la caja y si la caja está girada
+        if (!$(event.target).closest('.caja').length && $('.caja').hasClass('girada')) {
+            $('.caja').removeClass('girada');
+        }
     });
 
-    projectExit.on('click', () => {
-        $('.project_modal').hide();
+    // cambio de tarjeta (frontal/trasera)
+    $('.caja').on('click', function() {
+        $(this).toggleClass("girada");
     })
-
-    modal.click((event) => {
-        if (event.target === modal[0]) modal.hide();
-    })  
 });
